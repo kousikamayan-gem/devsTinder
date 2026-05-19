d762416aabe2edf78aee0678de42839a9aea94b0
kousika <kousikamayan@gmail.com>
2026-05-19 21:40:33 +0530

    initialized dev tiner project

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.use("/test",(req,res) => {
+    res.send('Hello World');
+})
+app.listen(7777, () => {
+    console.log('Server is running on port 7777');
+})
